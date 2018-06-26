cb9862263bd2254278fafdb284f0af6cf140b61f
shaygyawali <shaygyawali@gmail.com>
2018-06-26 14:07:16 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -1,9 +1,9 @@
 class App < Sinatra::Base
 
-  # Define your GET '/' route below and respond with "Hello, World!"
-
-end
-
 get "/" do  
   "Hello, World!"
+end  # Define your GET '/' route below and respond with "Hello, World!"
+
 end
+
+
